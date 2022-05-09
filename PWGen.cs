e999086b3a8c02a39973dade6298fe1f8e081f87
30jannik06 <80638124+30jannik06@users.noreply.github.com>
2022-05-09 09:32:52 +0200

    Added Comments

--- PWGen.cs
+++ PWGen.cs
@@ -4,7 +4,7 @@ namespace PasswortGenerator
 {
     public class PWGen
     {
-
+        // This method generates the password.
         public static void GeneratePW(int counter)
         {
             Random random = new Random();
@@ -14,8 +14,9 @@ namespace PasswortGenerator
             characters.AddRange(Enumerable.Range('a', 26).Select(i => (char)i));
             characters.AddRange(Enumerable.Range('A', 26).Select(i => (char)i));
             characters.AddRange(Enumerable.Range('0', 10).Select(i => (char)i));
+            characters.AddRange(Enumerable.Range('!', 16).Select(i => (char)i));
 
-            if (counter == 0)
+            if (counter <= 0)
             {
 
                 MessageBox.Show("Count musst be 1-24!");
@@ -27,7 +28,10 @@ namespace PasswortGenerator
                 {
                     password.Append(characters[random.Next(characters.Count)]);
                 }
+                // Can be used to display the generated password in pop-up.
                 // MessageBox.Show(password.ToString());
+
+                // Used to update the password output field
                 PasswordForm._PasswordForm.update("" + password.ToString());
             }
         }
